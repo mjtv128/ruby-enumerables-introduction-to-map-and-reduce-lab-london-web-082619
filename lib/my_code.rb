624cf85e2bbb283573f2093552532500bfd4d6a3
jocular-source-7570 <mjtvan@gmail.com>
2019-08-22 16:49:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,6 +23,13 @@ def reduce_to_total(source_array, starting_point =0)
   end 
 end 
 
+def reduce_to_total(source_array, starting_point =0)
+   i = 0
+    while i < source_array.length
+     i
+      i += 1
+    end
+
 def reduce_to_all_true(source_array)
   source_array.map{|x| !!x}
 end 
